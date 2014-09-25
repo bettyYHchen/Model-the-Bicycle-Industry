aa99bd039bbd2ca18f5aa9511bb9e3c326e71851
Betty Chen <betty_c@outlook.com>
2014-09-25 17:30:59 -0400

    Create Bike_business.py

---
+++ Bike_business.py
@@ -0,0 +1,20 @@
+class Bicycle(object):
+	def __init__(self,name,weight,cost):
+		self.name=name
+		self.weight=weight
+		self.cost=cost
+
+class BikeShops(object):
+	def __init__(self,name,inventory,margin):
+		self.name=name
+		self.inventory=inventory
+		self.margin=margin
+	def get_profit(self):
+		sum(margin)
+
+class Customers(object):
+	def __init__(self,name,fund,own):
+		self.name=name
+		self.fund=fund
+		self.own=own
+
